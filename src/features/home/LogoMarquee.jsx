b09f9d4bfb2c0e0be12910f18b9cfb4cc8d461af
jianb14 <jbacayon14@gmail.com>
2026-06-20 03:12:25 +0800

    fix logo

--- src/features/home/LogoMarquee.jsx
+++ src/features/home/LogoMarquee.jsx
@@ -1,10 +1,14 @@
 import LogoLoop from "../../components/ui/LogoLoop.jsx";
 import APA from "/src/assets/apa.png"
+import MHA from "/src/assets/mha.png"
+import NIHM from "/src/assets/nihm.png"
+import WHO from "/src/assets/who.png"
+
 const logos = [
     { src: APA, alt: 'Logo 1' },
-    { src: '/src/assets/mha.png', alt: 'Logo 2' },
-    { src: '/src/assets/nihm.jpg', alt: 'Logo 3' },
-    { src: '/src/assets/who.png', alt: 'Logo 4' },
+    { src: MHA, alt: 'Logo 2' },
+    { src: NIHM, alt: 'Logo 3' },
+    { src: WHO, alt: 'Logo 4' },
 ];
 
 export default function LogoMarquee() {
