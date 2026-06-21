9c5ed4af21b06e8d40dfbe2a30beaf0e33ea397f
jianb14 <jbacayon14@gmail.com>
2026-06-21 14:52:31 +0800

    change logo loop

--- src/features/home/LogoMarquee.jsx
+++ src/features/home/LogoMarquee.jsx
@@ -1,4 +1,5 @@
 import LogoLoop from "../../components/ui/LogoLoop.jsx";
+import { Award, ShieldCheck, Lock } from "lucide-react";
 import APA from "/src/assets/apa.svg"
 import MHA from "/src/assets/mha.svg"
 import NIHM from "/src/assets/nimh.svg"
@@ -16,7 +17,7 @@ const logos = [
 export default function LogoMarquee() {
     return (
         <section className="relative bg-bg-primary w-full">
-            <div className="py-20 sm:pt-0 sm:px-10 md:px-30 lg:px-50">
+            <div className="py-40 sm:pt-0 sm:px-10 md:px-30 lg:px-50">
                 <h4 className="text-blue mb-12 text-center font-medium mx-4">Trusted by Individuals, Families, and Organizations</h4>
                 
                 <LogoLoop
@@ -29,6 +30,20 @@ export default function LogoMarquee() {
                     fadeOut
                 />
 
+                <div className="flex justify-center gap-4 mt-18 flex-wrap">
+                    <div className="flex items-center gap-1 bg-blue-ghost relative z-5 py-1.5 px-2.5 rounded-2xl">
+                        <ShieldCheck size={16} className="text-blue"/>
+                        <p className="text-xs font-medium text-gray-500">HIPAA Compliant</p>
+                    </div>
+                    <div className="flex items-center gap-1 bg-blue-ghost relative z-5 py-1.5 px-2.5 rounded-2xl">
+                        <Award size={16} className="text-blue"/>
+                        <p className="text-xs font-medium text-gray-500">APA Accredited</p>
+                    </div>
+                    <div className="flex items-center gap-1 bg-blue-ghost relative z-5 py-1.5 px-2.5 rounded-2xl">
+                        <Lock size={15} className="text-blue"/>
+                        <p className="text-xs font-medium text-gray-500">256-bit SSL Encrypted</p>
+                    </div>
+                </div>
             </div>
         </section>
         
